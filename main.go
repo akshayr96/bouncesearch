21eefe5cee9ab0bb096b0488ef480d1631470fd5
Akshay Ramesh <akshayrameshbabu@gmail.com>
2019-11-16 14:44:00 +0530

    Added unit tests

--- main.go
+++ main.go
@@ -1,42 +1,10 @@
 package main
 
-import (
+import "fmt"
 
-	// "log"
-	// "net/http"
-
-	"fmt"
-
-	"github.com/akshayr96/bounceSearch/database"
-	"github.com/akshayr96/bounceSearch/provider"
-	"github.com/akshayr96/bounceSearch/types"
-	"github.com/akshayr96/bounceSearch/utils"
-)
+// "log"
+// "net/http"
 
 func main() {
-	tree := &types.Node{}
-
-	//Inserting words into a ternary tree
-	utils.InsertWord(tree, "app", "6516516516", 5.5)
-	utils.InsertWord(tree, "app", "561919189", 5.4)
-	utils.InsertWord(tree, "apple", "161641640", 1.0)
-	utils.InsertWord(tree, "bat", "56316516516", 0.5)
-
-	// Searching for a words in the tree
-	fmt.Println(types.CompleteMatch == utils.Parser(tree, "app"))
-	fmt.Println(types.NoMatch == utils.Parser(tree, "ball"))
-	fmt.Println(types.PartialMatch == utils.Parser(tree, "ap"))
-	fmt.Println(types.NoMatch == utils.Parser(tree, "x"))
-
-	provider.WriteTree(tree, "randomapp", "users")
-
-	//Getting the Levenshtien distance between two words
-	fmt.Println(utils.LevenshteinDistance("app", "map"))
-
-	// log.Fatal(http.ListenAndServe(":8080", routes.Routes()))
-
-	//API Example
-	conn := database.Connect("RandomApp")
-	users := conn.Collection("users")
-	users.Me()
+	fmt.Println("Under construction")
 }
---
+++ main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/akshayr96/bounceSearch/provider"
+	"github.com/akshayr96/bounceSearch/types"
+	"github.com/akshayr96/bounceSearch/utils"
+)
+
+// Tests the indexer and the parser
+func TestIndexerAndParser(t *testing.T) {
+	tree := &types.Node{}
+	testWords := []string{"plumbus", "eyeholeman", "morty", "glibglob", "squanchy"}
+
+	//Indexing words
+	for _, word := range testWords {
+		utils.InsertWord(tree, word, "1", 1.0)
+	}
+
+	//writes the tree file for reference
+	provider.WriteTree(tree, "randomapp", "users")
+
+	//tests for complete match
+	for _, word := range testWords {
+		if utils.Parser(tree, word) != types.CompleteMatch {
+			t.Errorf("Complete match test failed for the word  %s", word)
+		}
+	}
+
+	//tests for partial match
+	partialMatchWords := []string{"plumb", "eye", "mort", "glib", "squanch"}
+	for _, word := range partialMatchWords {
+		if utils.Parser(tree, word) != types.PartialMatch {
+			t.Errorf("Partial match test failed for the word %s", word)
+		}
+	}
+
+	//tests for no match
+	noMatchWords := []string{"froopyland", "summer", "birdperson", "fakedoors", "shmoopydoop"}
+	for _, word := range noMatchWords {
+		if utils.Parser(tree, word) != types.NoMatch {
+			t.Errorf("No match test failed for the word %s", word)
+		}
+	}
+
+}
+
+func TestLevenshtein(t *testing.T) {
+	if utils.LevenshteinDistance("app", "map") != 2 {
+		t.Error("Levenshtein test failed")
+	}
+}
+
+// log.Fatal(http.ListenAndServe(":8080", routes.Routes()))
+
+//API Example
+// conn := database.Connect("RandomApp")
+// users := conn.Collection("users")
+// users.Me()
